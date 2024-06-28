80491103987e3cd4c61cd17b30e0b1e3752734dd
tianjili <tianjili@tesla.com>
2024-06-28 16:56:06 -0500

    config

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const config = require("config");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const logger = require("./middleware/logger");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost:27017/vidly")
@@ -13,6 +19,8 @@ mongoose
 const genres = require("./routes/genres");
 const home = require("./routes/home");
 const customers = require("./routes/customers");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 
 app.use(express.json());
 app.use(helmet());
@@ -21,6 +29,8 @@ app.use(logger);
 
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 app.use("/", home);
 
 app.listen(3000, () => console.log("Listening on port 3000..."));
