b98dd574c44fca8147a1caa139b2d35aeae9cf7d
TJ Li <TianjianLi314@gmail.com>
2024-07-06 10:33:54 -0500

    epic

--- index.js
+++ index.js
@@ -1,71 +1,12 @@
-require("express-async-errors");
 const winston = require("winston");
-// require("winston-mongodb");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-const config = require("config");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const logger = require("./middleware/logger");
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
 
-// process.on("uncaughtException", (ex) => {
-//   winston.error(ex.message, ex);
-//   process.exit(1);
-// });
+require("./startup/logging");
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-// winston.add(winston.transports.MongoDB, {
-//   db: "mongodb://localhost:27017/vidly",
-// });
-
-// const p = Promise.reject(new Error("Something failed miserably!"));
-// p.then(() => console.log("Done"));
-
-throw new Error("FAILED");
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://localhost:27017/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Couldn't connect to mongo", err));
-
-const genres = require("./routes/genres");
-const home = require("./routes/home");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
-
-app.use(express.json());
-app.use(helmet());
-app.use(morgan("tiny"));
-app.use(logger);
-
-app.use("/", home);
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
-
-app.listen(3000, () => console.log("Listening on port 3000..."));
+const port = process.env.PORT || 3000;
+app.listen(3000, () => winston.info("Listening on port 3000..."));
